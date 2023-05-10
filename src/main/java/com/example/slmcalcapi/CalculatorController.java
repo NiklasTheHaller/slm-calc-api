22d9ecb1d2fe34bea0d381bd57fd5bee108c2f07
Niklas <wi22b066@technikum-wien.at>
2023-05-10 14:42:22 +0200

    added addition feature

--- src/main/java/com/example/slmcalcapi/CalculatorController.java
+++ src/main/java/com/example/slmcalcapi/CalculatorController.java
@@ -1,2 +1,17 @@
-package com.example.slmcalcapi;public class CalculatorController {
+package com.example.slmcalcapi;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+
+    @RequestMapping("/calc/sum")
+    public int sum(
+            @RequestParam int a,
+            @RequestParam int b){
+    return a + b;
+    }
+
 }
