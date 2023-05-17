85db17929f945c5a4585162fef5646f7920ab764
Niklas <wi22b066@technikum-wien.at>
2023-05-17 10:01:01 +0200

    add subtraction feature

--- src/main/java/com/example/slmcalcapi/CalculatorController.java
+++ src/main/java/com/example/slmcalcapi/CalculatorController.java
@@ -7,11 +7,50 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CalculatorController {
 
+    /**
+     * Add two integers.
+     *
+     * @param a first integer
+     * @param b second integer
+     */
+
+
     @RequestMapping("/calc/sum")
     public int sum(
             @RequestParam int a,
             @RequestParam int b){
     return a + b;
     }
+
+    /**
+     * Multiply two integers.
+     *
+     * @param a first integer
+     * @param b second integer
+     */
+
+
+    @RequestMapping("/calc/product")
+    public int product(
+            @RequestParam int a,
+            @RequestParam int b){
+        return a * b;
+    }
+
+    /**
+     * subtract two integers.
+     *
+     * @param a first integer
+     * @param b second integer
+     */
+
+
+    @RequestMapping("/calc/subtract")
+    public int subtract(
+            @RequestParam int a,
+            @RequestParam int b){
+        return a - b;
+    }
+
 
 }
---
+++ src/test/java/com/example/slmcalcapi/CalculatorControllerTest.java
@@ -0,0 +1,51 @@
+package com.example.slmcalcapi;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorControllerTest {
+
+    @Test
+    void sumTest1(){
+        // Arrange
+        CalculatorController calculatorController = new CalculatorController();
+
+        int a = 5;
+        int b = 4;
+
+        // Act
+        int result = calculatorController.sum(a, b);
+
+        // Assert
+        assertEquals(9, result);
+
+    }
+
+    @Test
+    void sumTest2(){
+        CalculatorController calculatorController = new CalculatorController();
+
+        int a = -10;
+        int b = 10;
+
+        int result = calculatorController.sum(a, b);
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    void productTest(){
+        CalculatorController calculatorController = new CalculatorController();
+
+        int a = 4;
+        int b = 5;
+
+        int result = calculatorController.product(a, b);
+
+        assertEquals(20, result);
+
+    }
+
+
+}
